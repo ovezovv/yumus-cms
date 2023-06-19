3b9664ffcd7aa58517b4702009ec817a797f2ced
Orazmammet Annayew <annayeworaz2004@gmail.com>
2023-06-19 15:25:36 +0500

    kop zatlary

--- src/components/RecentOrders.tsx
+++ src/components/RecentOrders.tsx
@@ -1,28 +1,34 @@
-import React from 'react'
-import {data} from '../data/data'
-import {FaShoppingBag} from 'react-icons/fa'
+import React from "react";
+import { data } from "../data/data";
+import { FaShoppingBag } from "react-icons/fa";
 
 const RecentOredrs = () => {
   return (
-    <div className='w-full md:col-span-2 relative lg:-[70vh] h-[50vh]cm-auto p-4 gap-4 border rounded-lg bg-white overflow-scroll'>
+    <div className="ml-4 w-full col-span-1 relative lg:-[70vh] h-[50vh]cm-auto p-4 border rounded-lg bg-white overflow-scroll">
       <h1>RecentsOrders</h1>
       <ul>
-        {data.map((order,id)=>(
-          <li key={id}  className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 flex items-center cursor-pointter'>
-            <div className='bg-purple-100 rounded-lg p-3'>
-              <FaShoppingBag className ='text-purple-800' />
+        {data.map((order, id) => (
+          <li
+            key={id}
+            className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 flex items-center cursor-pointter"
+          >
+            <div className="bg-purple-100 rounded-lg p-3">
+              <FaShoppingBag className="text-purple-800" />
             </div>
-              <div className='pl-4'>
-                <p className='text-gray-800 font-bold'>${ order.total}</p>
-                <p text-gray-400 text-sm>{order.name.first}</p>
-              </div>
-              <p className='lg:flex md:hidden absolute right-6'>{order.date}</p>
-              <p className='lg:flex md:hidden absolute right-6'>{order.date}</p>
+            <div className="pl-4">
+              <p className="text-gray-800 font-bold">${order.total}</p>
+              <p text-gray-400 text-sm>
+                {order.name.first}
+              </p>
+            </div>
+            <p className="lg:flex md:hidden absolute right-6">{order.date}</p>
+
+            <div></div>
           </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default RecentOredrs
+export default RecentOredrs;
